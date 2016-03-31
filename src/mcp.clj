46f301ce4dff0a80f4fad34f44914ff5e012542d
Eric Chai <electromatter@gmail.com>
2016-03-31 15:31:55 -0400

    all but non-trivial types

--- src/mcp.clj
+++ src/mcp.clj
@@ -8,85 +8,137 @@
 
 (ns mcp)
 
-(require '[clojure.set])
-
-;machines here are defined as trees of expressions
-; match = branch
-; field = output
-; emit = leaf
-;
-; field is non-terminal
-; match is terminal
-; emit is terminal
-;
-; (match <type> <&matches>)
-; (field <&names> <type>)
-; (emit <name>)
-;
-; machines are:
-; (<name> <root>)
-
-(def integer_types
-	#{"varint" "varlong" "bool" "byte" "short" "int" "long"})
-
-(defn integer_type? [typename]
-	(contains? integer_types (str typename)))
-
-(def parameterized_types
-	#{"array" "bytes"})
-
-(declare valid_type?)
-
-(defn valid_size? [size fields]
-	(or (integer? size)
-		(integer_type? size)
-		(integer_type? (get (reduce concat fields) size))))
-
-(def parameter_checkers {
-	"array" (fn [fields size elmtype]
-		(and	(valid_size? size fields)
-			(valid_type? elmtype fields)))
-	"bytes" (fn [fields size]
-		(or (= (str size) "eof")
-			(valid_size? fields size)))
-	})
-
-(def types (clojure.set/union integer_types parameterized_types
-	#{"float" "double" "string" "slot" "position" "angle" "uuid"}))
-
-(defn valid_type? [typename fields]
-	(or (contains? types (str typename))
-		(let [parameters (rest typename) typename (str (first typename))]
-		(and (contains? parameterized_types typename)
-			(apply (get parameter_checkers typename) (cons fields parameters))))))
-
-(defn build-fields [arg]
-	(let [names (butlast arg) typename (last arg)]
-		(assert (seq names))
-		(map #(list % typename) names)))
-
-(defn fold-schema
-	([root] (fold-schema root '()))
-	([root fields]
-		(let [node (first root) cont (rest root)]
-		(case (str (first node))
-		"match" (let [arg (second node) branches (nthrest node 2)]
-			(assert (empty? cont) (seq branches))
-			(assert (distinct? (map #(first %) branches)))
-			(assert (integer_type? arg))
-			(let [records (map #(fold-schema (rest %) fields) branches)]
-				(assert (distinct? (map #(first %) records)))
-				(reduce concat records)
-			))
-		"field" (do
-			(assert (seq cont))
-			(assert (valid_type? (last node) fields))
-			(fold-schema cont (concat fields (build-fields (rest node)))))
-		"emit" (let [name (second node)]
-			(assert (= (count node) 2))
-			(assert (distinct? (map #(first %) fields)))
-			(list (cons name fields)))))))
-
 (defn read-file [filename]
 	(read-string (str \( (slurp filename) \))))
+
+; returns true if a set of maps are disjoint i.e. have no keys in common
+(defn disjoint?
+	[& col]
+	(distinct? (apply concat (map keys col))))
+
+(defmulti translate
+	(fn
+		([expr] :initial)
+		([state expr] (first expr))))
+
+(def translate-file (comp translate read-file))
+
+; this sets up the state of the translator
+(defmethod translate :initial
+	[expr]
+	(reduce translate {} expr))			; visit each expr
+
+; the following take state and expr and return the new state
+
+; creates a new composite type
+(defmethod translate 'compose
+	[state expr]
+	(let [[_ typename & fields] expr]
+	(assert (not (contains? state typename)))	; ensure distinct name
+	(assert (not (state :cur)))			; ensure we are root
+	(let [state (assoc state typename :cur)		; reserve the name and visit fields
+		state (reduce translate
+			(assoc state :cur {:name typename})
+			fields)]
+	(dissoc (assoc state
+			:roots (cons typename (state :roots))
+			typename (state :cur))		; name the type
+		:cur :done :fields :order))))		; cleanup
+
+; constructs a scalar type if it doesn't exitst and verifies it was previously
+; used as a scalar retuns:
+;	(state scalar_typename)
+(defn buildscalar
+	[state typename]
+	(if (contains? state typename)
+		(do (assert ((state typename) :scalar)) (list state typename))
+		(list (assoc state typename {:name typename :scalar true}) typename)))
+
+; constructs a non-trivial type if nessicary and returns:
+;	(state simplified_typename)
+;TODO: non-trivial type
+(defn buildtype
+	[state typename]
+	(list state typename))
+
+; adds a field to the current type
+(defmethod translate 'field
+	[state expr]
+	(let [[_ & names] expr
+		typename (last names)
+		names (butlast names)
+		[state typename] (buildtype state typename); construct type
+		cur (state :cur)
+		fields (interleave names (repeat typename)); build fields
+		order (map #(do {:action :field :name % :type typename}) (take-nth 2 fields))
+		fields (apply hash-map fields)]
+		(assert cur)				; field without context?
+		(assert (not (state :done)))		; field after terminal!
+		(assert (disjoint? (state :fields) fields))
+		(assoc state
+			:cur (assoc cur
+				:fields (merge (cur :fields) fields)
+				:order (concat (cur :order) order))
+			:fields (merge (state :fields) fields))))
+
+; adds a literal to the type
+(defmethod translate 'literal
+	[state expr]
+	(let [[_ & values] expr
+		typename (last values)
+		[state typename] (buildscalar state typename)
+		values (butlast values)
+		cur (state :cur)
+		literals (map #(do {:action :literal :value % :type typename}) values)]
+		(assert cur)				; literal without context?
+		(assert (not (state :done)))		; literal after end?
+		(assoc state
+			:cur (assoc cur
+				:order (concat (cur :order) literals)))))
+
+; builds a scalar type from a typespec returns:
+; (state condition)
+;TODO: allow field references
+(defn buildcondition
+	[state condition]
+	(buildscalar state condition))
+
+; builds a branch into the current type
+(defmethod translate 'match
+	[state expr]
+	(let [[_ condition & branches] expr
+		[state condition] (buildcondition state condition)
+		cur (state :cur)]
+	(assert cur)					; make sure we are in a composite
+	(assert (not (state :done)))			; match after terminal symbol!
+	(assert (distinct? (map first branches)))	; ensure distinct branches
+	(assoc state
+		; append match
+		:cur (assoc cur
+			:order (concat (cur :order)	; add the condition marker and reduce branches
+				`(~{:action :match :on condition}))
+			:branches (apply hash-map (interleave
+				(map first branches)
+				(map #(get (reduce translate (assoc state :cur {}) (rest %)) :cur) branches))))
+		:done true)))
+
+; names a new terminal type
+(defmethod translate 'emit
+	[state expr]
+	(let [[_ newname] expr
+		cur (state :cur)]
+	(assert cur)					; make sure we are in a composite
+	(assert (not (state :done)))			; emit after terminal!
+	(assert (or
+		(and (not newname) (cur :name))
+		(and newname (not (cur :name)))))	; check if we need a name
+	(assert (not (contains? state newname)))	; ensure distinct name
+	(if newname
+		(assoc state
+			newname {:name newname :fields (state :fields) :order (state :order)}
+			:done true)
+		(assoc state :done true))))
+
+(use 'clojure.pprint)
+(pprint (translate-file (first *command-line-args*)))
 
